bc5adb174f5c9f6c7839f2f73ba70eaa7a178b8d
xulihang <xulihanghai@163.com>
2023-06-07 17:36:19 +0800

    display the image in the cropper element

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface ImageCropper {
+        "img"?: HTMLImageElement;
     }
 }
 declare global {
@@ -22,6 +23,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface ImageCropper {
+        "img"?: HTMLImageElement;
     }
     interface IntrinsicElements {
         "image-cropper": ImageCropper;
--- src/components/image-cropper/image-cropper.tsx
+++ src/components/image-cropper/image-cropper.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, Prop, State, Watch, h } from '@stencil/core';
 
 @Component({
   tag: 'image-cropper',
@@ -6,10 +6,24 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class ImageCropper {
-
+  @Prop() img?: HTMLImageElement;
+  @State() viewBox:string = "0 0 1280 720";
+  @Watch('img')
+  watchPropHandler(newValue: HTMLImageElement) {
+    if (newValue) {
+      this.viewBox = "0 0 "+newValue.naturalWidth+" "+newValue.naturalHeight;
+    }
+  }
   render() {
     return (
       <Host>
+        <svg 
+          version="1.1" 
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox={this.viewBox}
+        >
+          <image href={this.img ? this.img.src : ""}></image>
+        </svg>
         <slot></slot>
       </Host>
     );
